d8f76d5bc7d9374707b61415c42afe1bb7f85074
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-08-05 00:41:45 +0100

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
 // src/utils/api.js
-export const BASE_URL =
- 'https://voicesoncanvas1.onrender.com'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL 
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
