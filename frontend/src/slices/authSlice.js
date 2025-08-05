b870f3cab67a60250db655b76938a96bb4ee6756
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-08-05 23:54:54 +0100

    Update authSlice.js

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  userInfo: null, // Don't persist sensitive data
-};
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
+}
 
 const authSlice = createSlice({
   name: 'auth',
