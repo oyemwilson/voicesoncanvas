8c238fec952568de4f8b3e0c35052b2fb7655a72
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-10-25 08:58:51 +0100

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -134,7 +134,7 @@ const newSalesCount       = newSales.length;
       id="currency"
       value={currency}
       onChange={(e) => setCurrency(e.target.value)}
-      className="border rounded px-0 py-0 text-sm bg-[#ffefc0]  text-gray-800 focus:outline-none focus:ring-1 focus:ring-yellow-500"
+      className="border rounded px-1 py-0 text-sm bg-[#ffefc0]  text-gray-800 focus:outline-none focus:ring-1 focus:ring-yellow-500"
     >
       <option value="NGN">₦ NGN</option>
       <option value="USD">$ USD</option>
